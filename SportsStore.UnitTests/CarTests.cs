80159d0997922e690dd0e00e042a945be73a6417
Alexandr Alexandrov <aliaxandr.aliaxandrov@gmail.com>
2017-08-07 22:13:42 +0300

    First commit

---
+++ SportsStore.UnitTests/CarTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            //arrange
+
+            Product p1 = new Product {ProductID = 1, Name = "P1"};
+            Product p2 = new Product {ProductID = 2, Name = "P2"};
+            Cart target=new Cart();
+
+            //act
+            target.AddItem(p1,1);
+            target.AddItem(p2,2);
+            CartLine[] result = target.Lines.ToArray();
+            //assert 
+            Assert.AreEqual(result.Length,2);
+            Assert.AreEqual(result[0].Product,p1);
+            Assert.AreEqual(result[1].Product, p2);
+            
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            //arrange
+
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Cart target = new Cart();
+
+            //act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] result = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+
+            //assert 
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Quantity, 11);
+            Assert.AreEqual(result[1].Quantity, 1);
+
+        }
+
+        [TestMethod]
+        public void Can_remove_Line()
+        {
+            //arrange
+
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+            Cart target = new Cart();
+
+            //act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+            target.RemoveLine(p2);
+
+            //assert 
+            Assert.AreEqual(target.Lines.Where(c=>c.Product==p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
+
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            //arrange
+
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M};
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M};
+
+            Cart target = new Cart();
+
+            //act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+
+            decimal result=target.ComputeTotalValue();
+
+            //assert 
+            Assert.AreEqual(result, 450M);
+
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            //arrange
+
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+
+            //act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            target.Clear();
+
+            //assert 
+            Assert.AreEqual(target.Lines.Count(), 0);
+
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToRouteResult AddToCart(int id, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == id);
+            if (product!=null)
+            {
+                GetCart().AddItem(product,1);
+            }
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToRouteResult RemoveToCart(int id, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == id);
+            if (product!=null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart) Session["Cart"];
+            if (cart==null)
+            {
+                cart=new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        
+    }
+}
